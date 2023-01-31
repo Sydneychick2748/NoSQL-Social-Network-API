3f2048bda9b939b8f7b3a4d2f72a1e351facb969
ana <ana@anniecannons.com>
2023-01-31 09:32:35 -0800

    added some notes peudocoding

---
+++ models/User.js
@@ -0,0 +1,23 @@
+Models
+User
+
+username
+
+String
+Unique
+Required
+Trimmed
+email
+
+String
+Required
+Unique
+Must match a valid email address (look into Mongoose's matching validation)
+thoughts
+
+Array of _id values referencing the Thought model
+friends
+
+Array of _id values referencing the User model (self-reference)
+
+create a virtual called friendCount that retrieves the length of the user's friends array field on query.
